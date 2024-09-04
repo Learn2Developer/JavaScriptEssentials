18e2c654bea38b62c2cccb94b2add44d34721f4a
Learn2Developer <jwag1234@hotmail.com>
2024-09-03 20:36:11 -0400

    added site feedback box

--- jscoursetesting/productSurvey/product_survey.js
+++ jscoursetesting/productSurvey/product_survey.js
@@ -14,6 +14,8 @@ const job = document.getElementById('job').value;
 const designation = document.getElementById('designation').value;
 const productType = document.getElementById('productType').value;
 const feedback = document.getElementById('feedbackText').value;
+const userExperience = document.getElementById('userExperience').value;
+
 
 document.getElementById('userName').innerHTML = username;
 document.getElementById('userAge').innerHTML = age;
@@ -22,6 +24,7 @@ document.getElementById('userJob').innerHTML = job;
 document.getElementById('userDesignation').innerHTML = designation;
 document.getElementById('userProductChoice').innerHTML = productType;
 document.getElementById('userFeedback').innerHTML = feedback;
+document.getElementById('experience').innerHTML = userExperience;
 document.getElementById('userInfo').style.display = 'block';
 
 alert('Thank you for your valuable feedback')
